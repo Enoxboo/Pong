79482ddfcffb6efbdc7120c899405181dc5f8795
Enoxboo <matteo.marquant@ynov.com>
2025-06-08 18:44:04 +0200

    Final Game

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
-@export var speed := -300
+@export var speed := -500
 var angle := 90
 @export var score : RichTextLabel
+@export var winner_text : RichTextLabel
 var score1 := 0
 var score2 := 0
 var win := true
+
 @export var victory_screen : Control
+@export var main_screen : Control
+
 
 func _ready() -> void:
 	update_score(0, 0)
+	victory_screen.visible = false
+	main_screen.visible = true
 
 func _process(delta: float) -> void:
 	if win:
@@ -23,41 +29,55 @@ func _process(delta: float) -> void:
 	
 	if position.x < 0:
 		reset_position()
-		update_score(1, 0)
+		update_score(0, 1)
 		
 	if position.x > 640:
 		reset_position()
-		update_score(0, 1)
-	
+		update_score(1, 0)
+		
 
 func reset_position():
 	position.x = 320
 	position.y = 240
+	speed = 300
+	angle = 90
 
 func update_score(point1, point2):
 	score1 += point1
 	score2 += point2
 	score.text = str(score1) + " - " + str(score2)
-	if score1 == 10 or score2 == 10:
-		win_game()
+	if score1 == 10:
+		win_game("Player 1")
+	elif score2 == 10:
+		win_game("Player 2")
 
-func win_game():
+func win_game(winner):
 	reset_position()
 	win = true
+	winner_text.text = winner + " won"
 	victory_screen.visible = true
 	
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	var new_angle = angle
+	var new_speed = speed
 	var raquette_y = area.global_position.y
 	var ball_y = position.y
 	
-	angle = ball_y - raquette_y
-
-	speed *= -1.05
+	angle = (ball_y - raquette_y)*10
+	speed *= -1
+	
+	angle = clamp(new_angle, -300, 300)
 
 
 func _on_button_button_down() -> void:
 	victory_screen.visible = false
+	main_screen.visible = false
 	win = false
 	score1 = 0
 	score2 = 0
 	update_score(0, 0)
+
+
+func _on_button_2_button_down() -> void:
+	victory_screen.visible = false
+	main_screen.visible = true
--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,7 +1,53 @@
 extends Node2D
 
-@export var balle : Node2D 
-var speed := 200
+@export var balle : Node2D
+var speed := 100 
+var vision_distance := 370 
+var difficulty
+var can_move := false
+var two_players := false
 
-func _process(delta: float) -> void:
-	position.y = balle.position.y
+func _physics_process(delta: float) -> void:
+	if two_players:
+		speed = 200
+		var new_position = position.y
+
+		if Input.is_action_pressed("down2"):
+			new_position += speed * delta
+		if Input.is_action_pressed("up2"):
+			new_position -= speed * delta
+
+		position.y = clamp(new_position, 60, 420)
+	else:
+		check_movement()
+		if not can_move:
+			return
+
+		var new_position = position.y
+		if balle.position.y > position.y:
+			new_position += speed * delta
+		else:
+			new_position -= speed * delta
+
+		position.y = clamp(new_position, 60, 420)
+
+func check_movement() -> void:
+	if balle.position.x > vision_distance:
+		can_move = true
+	else:
+		can_move = false
+
+func _easy() -> void:
+	speed = 75
+	vision_distance = 420
+
+func _medium() -> void:
+	speed = 100
+	vision_distance = 370
+
+func _hard() -> void:
+	speed = 125
+	vision_distance = 320
+
+func _on_button_4_button_up() -> void:
+	two_players = true
